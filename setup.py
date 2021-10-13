d2457874653de1a1dcd2a1d41e36c8a9eeabd3fb
Misha133 <mihagribkov133@gmail.com>
2021-10-13 13:01:17 +0300

    v 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ActivitiesBot',
-    version='0.0.1',
+    version='0.1.1',
     packages=['discord-py-slash-command', 'discord', 'discord-together'],
     url='',
     license='',
